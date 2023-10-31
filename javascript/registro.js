06535da657319f1684329b9eebaac39103c535a3
Romboscuro <mgv76@alu.ua.es>
2023-10-31 19:59:30 +0100

    Todo hecho aunque sin probar demasiado

--- javascript/registro.js
+++ javascript/registro.js
@@ -13,38 +13,87 @@ window.addEventListener('load', () => {
     let correo_input = document.getElementById('correo');
     let fecha_input = document.getElementById('fecha-nacimiento');
 
-// ---------- VALIDAMOS LOS DATOS DEL REGISTRO ---------------------
+    let boton_registro = document.getElementById('btn-registro');
+// ---------- LISTENERS DE LOS INPUTS ---------------------
+// NOMBRE
 nombre_input.addEventListener('blur', () => {
     let nombre = nombre_input.value;
     nombre_valido = validarNombre(nombre);
     console.log(nombre_valido);
+
+    // Mostramos mensaje
+    if(!nombre_valido) ponerMensaje(0,"El nombre de usuario no es válido");
 });
 
+nombre_input.addEventListener('click', () => {
+    borrarMensaje(nombre_input);
+});
+
+// CONTRASEÑA
 contrasenya_input.addEventListener('blur', () => {
     let contrasenya = contrasenya_input.value;
     contrasenya_valida = validarContrasenya(contrasenya);
     console.log(contrasenya_valida);
+
+    // Mostramos mensaje
+    if(!contrasenya_valida) ponerMensaje(1,"Pon al menos una mayúscula, minúscula y número");
 });
 
+contrasenya_input.addEventListener('click', () => {
+    borrarMensaje(contrasenya_input);
+});
+
+// REPETIR CONTRASEÑA
 contrasenya_rep_input.addEventListener('blur', () => {
     let contrasenya = contrasenya_input.value;
     let contrasenya_rep = contrasenya_rep_input.value;
 
     // Hacemos la comprobacion 
     contrasenya_rep_valida = (contrasenya === contrasenya_rep);
+
+    // Mostramos mensaje
+    if(!contrasenya_rep_valida) ponerMensaje(2,"Repite lo que has puesto arriba");
+
     console.log(contrasenya_rep_valida);
 });
 
+contrasenya_rep_input.addEventListener('click', () => {
+    borrarMensaje(contrasenya_rep_input);
+});
+
+// CORREO
 correo_input.addEventListener('blur', () => {
     let correo = correo_input.value;
     correo_valido = validarCorreo(correo);
     console.log(correo_valido); 
+
+    // Mostramos mensaje
+    if(!correo_valido) ponerMensaje(3,"El formato debe ser parte-local@dominio");
 });
 
+correo_input.addEventListener('click', () => {
+    borrarMensaje(correo_input);
+});
+
+// FECHA
 fecha_input.addEventListener('blur', () => {
     let fecha = fecha_input.value;
     fecha_valida = validarFecha(fecha);
     console.log(fecha_valida); 
+
+    // En este caso, el mensaje se muestra en el propio metodo de validacion, por haber varios mensajes
+});
+
+fecha_input.addEventListener('click', () => {
+    borrarMensaje(fecha_input);
+});
+
+// ------------ EVENTO DEL BOTON REGISTRARSE --------------
+boton_registro.addEventListener('click', function(event){
+
+    console.log(nombre_valido+" "+contrasenya_valida+" "+contrasenya_rep_valida+" "+correo_valido+" "+fecha_valida);
+    if(!(nombre_valido && contrasenya_valida && contrasenya_rep_valida && correo_valido && fecha_valida))
+    event.preventDefault();
 });
 
 });
@@ -185,8 +234,11 @@ function validarFecha(fec){
     let partes_fecha = fec.split('/');
 
     // Comprobar que hay tres partes
-    if(partes_fecha.length != 3) return false;
-
+    if(partes_fecha.length != 3){
+        // Mostramos mensaje de formato erroneo
+        if(!fecha_valida) ponerMensaje(5,"El formato debe ser dd/mm/yyyy");
+        return false;
+    }
     // Reordenar fecha y comprobar que es valida
     let fecha = partes_fecha.reverse().join('-');
 
@@ -194,7 +246,37 @@ function validarFecha(fec){
     if(isNaN(fechaValida)) return false;
 
     // Comprobamos si el usuario es mayor de edad
-    if(new Date().getFullYear() - partes_fecha[0] < 18) return false;
-
+    if(new Date().getFullYear() - partes_fecha[0] < 18){
+        // Mostramos mensaje de edad no permitida
+        if(!fecha_valida) ponerMensaje(5,"Debes tener 18 años o más");
+        return false;
+    }
     return true;
+}
+
+
+// -------------- FUNCION PARA PONER UN MENSAJE ABAJO DEL INPUT ------------
+/**
+ * 
+ * @param {int} index 
+ * @param {string} contenido 
+ */
+function ponerMensaje(index, contenido){
+    // Pillamos el div de la clase dependiendo del index
+    let label = document.getElementsByClassName('omrs-input-filled')[index];
+    // console.log(label);
+
+    // Creamos un span con la clase y lo ponemos debajo del label
+    let mensaje = document.createElement('span');
+    mensaje.className = 'omrs-input-helper';
+    mensaje.textContent = contenido;
+
+    label.appendChild(mensaje);
+}
+
+// BORRAR MENSAJE
+function borrarMensaje(input){
+    let parentLabel = input.parentElement;
+    let mensaje = parentLabel.getElementsByClassName('omrs-input-helper');
+    if(mensaje.length != 0) mensaje[0].remove();
 }
