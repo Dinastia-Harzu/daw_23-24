bb6590b55679a35c35d21d7176fe1577002cee3d
Dinastia-Harzu <dinastiadelah@gmail.com>
2023-11-01 12:11:05 +0100

    Diálogos hechos

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,11 @@
 window.addEventListener('load', () => {
     // Pilla el primero, que es el que quiero
     const opcion_login = document.querySelector('#tab-login > ul > li');
-    opcion_login.addEventListener('click', () => abrirDialogo(mostrarDialogoLogin, 'login'));
+    opcion_login.addEventListener('click', () => mostrarDialogoLogin());
 
     const articulos = document.querySelectorAll('.grid-img article');
     articulos.forEach(articulo => {
-        articulo.addEventListener('click', () => abrirDialogo(mostrarDialogoError, 'error'));
+        articulo.addEventListener('click', () => mostrarDialogoError());
     });
 
     [].slice.call(document.getElementsByTagName('dialog')).forEach(() => {
