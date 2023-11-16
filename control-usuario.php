dc21e19f9bef7962995731ef9c053ad37db9b6d6
Dinastia-Harzu <dinastiadelah@gmail.com>
2023-11-16 22:20:09 +0100

    Refactorización + cookie

--- control-usuario.php
+++ control-usuario.php
@@ -1,31 +1,30 @@
 <?php
 
-    $usuarios = array(
-        'ADMIN' => 'ADMIN123',
-        'DEBUG' => 'DEBUG',
-        'agrg11' => 'contraseña',
-        'mgv' => 'password'
-    );
-    $host = 'localhost';
-    $uri = 'daw_23-24';
-    
-    if(!isset($_POST['nombre']) || !isset($_POST['contraseña'])) {
-        $pagina = 'index.no_registrado.html';
-        header("Location: http://$host/$uri/$pagina");
-        exit;
-    }
+$usuarios = array(
+    'ADMIN' => 'ADMIN123',
+    'DEBUG' => 'DEBUG',
+    'agrg11' => 'clavesecreta',
+    'mgv' => 'password'
+);
+$protocolo = 'http://';
+$host = 'localhost';
+$uri = 'daw_23-24';
 
+if(isset($_POST["nombre"]) && isset($_POST["contraseña"])) {
     $nombre = $_POST["nombre"];
     $contraseña = $_POST["contraseña"];
-
     if(isset($usuarios[$nombre]) && array_search($contraseña, $usuarios, true)) {
-        $pagina = 'usuario.html';
-        header("Location: http://$host/$uri/$pagina");
-        exit;
+        $pagina = 'usuario.php';
     } else {
-        $pagina = 'index.no_registrado.html';
-        header("Location: http://$host/$uri/$pagina");
-        exit;
+        $pagina = 'index.no_registrado.php';
     }
+    if(isset($_POST["recuerdame"])) {
+        setcookie("recuerdame", $_POST["nombre"] . "¬" . $_POST["contraseña"], time() + 24 * 60 * 60 * 90);
+        echo $_COOKIE["recuerdame"];
+    }
+} else {
+    $pagina = 'index.no_registrado.php';
+}
+header("Location: $protocolo$host/$uri/$pagina");
 
 ?>
