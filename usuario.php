9ea42283d13aa704e99dbe6a68b0b8db4967d89f
Dinastia-Harzu <dinastiadelah@gmail.com>
2023-11-17 11:12:53 +0100

    Control de seguridad

--- usuario.php
+++ usuario.php
@@ -1,5 +1,8 @@
 <?php
     session_start();
+    if(!isset($_SESSION["usuario"])) {
+        header("Location: index.no_registrado.php");
+    }
     $tema = isset($_SESSION["tema"]) ? $_SESSION["tema"] : "oscuro";
 ?>
 <!DOCTYPE html>
