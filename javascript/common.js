cd53df7fb4e3892ea24cb583adee93f8f52d378e
Dinastia-Harzu <dinastiadelah@gmail.com>
2023-11-23 23:49:49 +0100

    Cookies y temas

--- javascript/common.js
+++ javascript/common.js
@@ -1,3 +1,11 @@
+const estilos = {
+    'oscuro': 'Modo oscuro (predeterminado)',
+    'claro': 'Modo claro',
+    'alto-contraste': 'Modo de alto contraste',
+    'letra-mayor': 'Modo de tipo de letra mayor',
+    'letra-mayor-y-alto-contraste': 'Modo de letra mayor y alto contraste'
+};
+
 /**
  * 
  * @param {string} etiqueta 
@@ -42,29 +50,46 @@ function validarFormulariolLogin(evt) {
 }
 
 window.addEventListener('load', () => {
-    const estilos = {
-        'oscuro': 'Modo oscuro (predeterminado)',
-        'claro': 'Modo claro',
-        'alto-contraste': 'Modo de alto contraste',
-        'letra-mayor': 'Modo de tipo de letra mayor',
-        'letra-mayor-y-alto-contraste': 'Modo de letra mayor y alto contraste'
-    };
     const select = $('busqueda-rapida').parentElement.appendChild(crearElemento('select', {}, 'change', evt => {
-        const link = (() => {
-            for(const estilo in estilos) {
-                const etiqueta = document.querySelector(`link[href="css/modos-alternativos/${estilo}.css"]`);
-                if(etiqueta) {
-                    return etiqueta;
-                }
-            }
-            return null;
-        })();
+        const link = recogerEtiquetaLink();
         const estilo = evt.target.value;
         link.setAttribute('href', `css/modos-alternativos/${estilo}.css`);
+        crearCookie('estilo', estilo, 45);
     }));
     for(const estilo in estilos) {
         select
             .appendChild(crearElemento('option', {value: estilo}))
                 .appendChild(document.createTextNode(estilos[estilo]));
     }
-})
+    const cookie = pillarCookie('estilo');
+    if(cookie.length > 0) {
+        select.value = cookie;
+        recogerEtiquetaLink().setAttribute('href', `css/modos-alternativos/${cookie}.css`);
+    }
+});
+
+function recogerEtiquetaLink() {
+    for(const estilo in estilos) {
+        const etiqueta = document.querySelector(`link[href="css/modos-alternativos/${estilo}.css"]`);
+        if(etiqueta) {
+            return etiqueta;
+        }
+    }
+    return null;
+}
+
+function crearCookie(nombre, valor, dias_caducidad) {
+    document.cookie = `${nombre}=${valor}; expires=${new Date(Date.now() + dias_caducidad * 24 * 60 * 60 * 1000).toUTCString()};path=`;
+}
+
+function pillarCookie(nombre) {
+    nombre += '=';
+    const cookies = decodeURIComponent(document.cookie).split(';');
+    for(const cookie of cookies) {
+        const c = cookie.trimStart();
+        if(c.startsWith(nombre)) {
+            return c.substring(nombre.length, c.length);
+        }
+    }
+    return "";
+}
