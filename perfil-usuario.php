01e293c9416aa23fe69a32c5b37f26d33d036bd0
Romboscuro <mgv76@alu.ua.es>
2023-11-28 20:33:30 +0100

    Perfil-usuario y empezando ver-album

--- perfil-usuario.php
+++ perfil-usuario.php
@@ -4,29 +4,71 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mastermatika - Perfil usuario</title>
+    <link rel="stylesheet" href="css/global/style.css">
+    <link rel="stylesheet" href="css/ordenador/style.css">
+    <link rel="stylesheet" href="css/tablet/style.css">
+    <link rel="stylesheet" href="css/movil/style.css">
+    <link rel="stylesheet" href="css/global/respuesta_usuario.css">
+    <link rel="stylesheet" href="css/ordenador/respuesta_usuario.css">
+    <link rel="stylesheet" href="css/tablet/respuesta_usuario.css">
+    <link rel="stylesheet" href="css/movil/respuesta_usuario.css">
+    <!--Añadir mas si eso luego-->
 </head>
 
 <?php
     include_once "inc/header.php";
-?>
 
-<body>
-        <section>
-        <h1>$mensaje</h1>
-            <div id="datos-usuario">
-                <p>Nombre de usuario: {$_POST["nombre"]}</p>
-                <p>Contraseña: {$_POST["contraseña"]}</p>
-                <p>Correo: {$_POST["correo"]}</p>
-                <p>Sexo: {$_POST["sexo"]}</p>
-                <p>Fecha de nacimiento: {$_POST["fecha-nacimiento"]}</p>
-                <p>Ciudad: {$_POST["ciudad"]}</p>
-            </div>
-        </section>
-</body>
+    // Select para obtener variables
+    $id = mysqli_connect("","root","","daw");
+    if(mysqli_connect_errno()) {
+        echo mysqli_connect_error();
+        exit();
+    }
 
-<?php
+    $result = mysqli_query($id,"
+        SELECT 
+            u.NomUsuario,
+            u.Foto,
+            u.FRegistro,
+            a.IdAlbum,
+            a.Titulo,
+            a.Descripcion
+        FROM albumes a 
+        JOIN usuarios u ON(a.Usuario = u.IdUsuario) 
+        WHERE u.IdUsuario = {$_GET["id"]}");
+
+    $row = mysqli_fetch_assoc($result);
+
+        echo <<<hereDOC
+                <body>
+                <main>
+                    <section>
+                    <h1>Perfil del usuario</h1>
+                        <div id="datos-usuario">
+                            <img href="{$row["Foto"]} alt="Foto de perfil">
+                            <p>Nombre de usuario: {$row["NomUsuario"]}</p>
+                            <p>Fecha de incorporación: {$row["FRegistro"]}</p>
+                            <hr>
+                            <h3>Lista de álbumes: </h3>
+            hereDOC;
+
+            mysqli_data_seek($result, 0);
+            while($row = mysqli_fetch_assoc($result)) {
+                echo <<<hereDOC
+                    <article>
+                        <p><a href = "ver-album.php?id={$row["IdAlbum"]}">{$row["Titulo"]}</a></p>
+                        <p>{$row["Descripcion"]}</p>
+                        <hr>
+                    </article>
+                hereDOC;
+            }
+                echo <<<hereDOC
+                        </div>
+                    </section>
+                </main>
+            </body>
+            hereDOC;
+        
     include_once "inc/footer.php";
 ?>
-
-
 </html>
