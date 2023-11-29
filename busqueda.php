cfed70532594b975de292c788cba9a9fb55e8400
Dinastia-Harzu <dinastiadelah@gmail.com>
2023-11-29 12:19:23 +0100

    Cambio en el formulario y otras refactorizaciones

--- busqueda.php
+++ busqueda.php
@@ -1,69 +1,58 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Descubrir - Masthermatika</title>
-    <link rel="stylesheet" href="css/global/style.css">
-    <link rel="stylesheet" href="css/ordenador/style.css">
-    <link rel="stylesheet" href="css/tablet/style.css">
-    <link rel="stylesheet" href="css/movil/style.css">
-    <link rel="stylesheet" href="css/global/busqueda.css">
-    <link rel="stylesheet" href="css/ordenador/busqueda.css">
-    <link rel="stylesheet" href="css/tablet/busqueda.css">
-    <link rel="stylesheet" href="css/movil/busqueda.css">
-    <link rel="alternate stylesheet" href="css/modos-alternativos/oscuro.css" title="Modo oscuro (predeterminado)">
-    <link rel="alternate stylesheet" href="css/modos-alternativos/claro.css" title="Modo claro">
-    <link rel="alternate stylesheet" href="css/modos-alternativos/alto-contraste.css" title="Modo de alto contraste">
-    <link rel="alternate stylesheet" href="css/modos-alternativos/letra-mayor.css" title="Modo de tipo de letra mayor">
-    <link rel="alternate stylesheet" href="css/modos-alternativos/letra-mayor-y-alto-contraste.css" title="Modo de letra mayor y alto contraste">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-<body>
 <?php
-    include_once "inc/header.php";
+    require_once "helpers/funciones.php";
 
-    // Select para obtener variables
-    $id = mysqli_connect("", "root", "", "daw");
-    if(mysqli_connect_errno()) {
-        echo mysqli_connect_error();
-        exit();
-    }
+    $conexion = abrirConexion();
 
-    $result = mysqli_query($id,"SELECT NomPais FROM paises");
+    generarHead('Descubrir - Masthermatika', 'oscuro');
 ?>
+<body>
+    <?php
+        include_once "inc/header.php";
+    ?>
     <main id="grid-params">
         <h1>Descubre</h1>
         <form action="resultado.php" method="post" class="tab-busc tab-params">
             <section >
                 <h2>Introduce los parámetros de búsqueda:</h2>
-                <p>
-                    <label for="titulo">Título: </label>
-                    <input type="text" name="titulo" id="titulo" placeholder="Introduce el título...">
-                </p>
-                <p>
-                    <label for="fecha-desde" class="tab-busc-fecha">Fecha entre:</label>
-                    <input type="date" name="fecha-desde" id="fecha-desde">
-
-                    <label for="fecha-hasta" class="tab-busc-y">y</label>
-                    <input type="date" name="fecha-hasta" id="fecha-hasta">
-                </p>
-                <p>
-                    <label for="paises">País: </label>
-                    <select name="pais" id="paises">
-                        <?php
-                            while($row = mysqli_fetch_array($result)) {
-                                echo <<<hereDOC
-                                    <option value = "{$row["NomPais"]}">{$row["NomPais"]}</option>
-                                hereDOC;
-                            }
-                        ?>
-                    </select>
-                </p>
+                <div class="omrs-input-group">
+                    <label class="omrs-input-filled">
+                        <input type="text" placeholder=" " name="titulo" id="titulo">
+                        <span class="omrs-input-label">Titulo</span>
+                    </label>
+                </div>
+                <div class="omrs-input-group">
+                    <label class="omrs-input-filled">
+                        <input type="date" placeholder=" " name="fecha-desde" id="fecha-desde">
+                        <span class="omrs-input-label">Desde</span>
+                    </label>
+                </div>
+                <div class="omrs-input-group">
+                    <label class="omrs-input-filled">
+                        <input type="date" placeholder=" " name="fecha-hasta" id="fecha-hasta">
+                        <span class="omrs-input-label">Hasta</span>
+                    </label>
+                </div>
+                <div class="omrs-input-group">
+                    <label class="omrs-input-filled">
+                        <select name="pais" id="paises">
+                            <option value="-1" selected disabled>-- Selecciona una opcion --</option>
+                            <?php
+                                if($resultado = $conexion->query("SELECT * FROM paises")) {
+                                    while($fila = $resultado->fetch_assoc()) {
+                                        $pais = $fila["NomPais"];
+                                        echo '<option value="' . $fila["IdPais"] . '">' . $pais . '</option>';
+                                    }
+                                    $resultado->free();
+                                }
+                            ?>
+                        </select>
+                        <span class="omrs-input-label">País</span>
+                    </label>
+                </div>
             </section>
-            <p id="busc_2">
-                <input type="submit" value="Enviar">
-            </p>
+            <div class="campo-boton-submit">
+                <button type="submit">Buscar</button>
+            </div>
         </form>
     </main>
 <?php
