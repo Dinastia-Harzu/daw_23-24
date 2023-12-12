eb657f5d50d62b9b83bc7ea581b6174c43189444
Romboscuro <mgv76@alu.ua.es>
2023-12-12 19:24:45 +0100

    configurar mvc completado

--- configurar.php
+++ configurar.php
@@ -1,40 +1,4 @@
 <?php
-    require_once "helpers/funciones.php";
-    session_start();
-
-    if($tema = $_POST["tema"] ?? $_SESSION["tema"] ?? false) {
-        $_SESSION["tema"] = $tema;
-    } else {
-        $tema = 'oscuro';
-    }
-
-    $conexion = abrirConexion();
-
-    generarHead('configurar', 'respuesta-usuario');
+require_once('db/db.php');
+require_once('controllers/configurar-controller.php');
 ?>
-<body>
-    <?php
-        require_once "inc/header.php";
-    ?>
-    <main>
-        <h2>Elige el estilo de la página</h2>
-        <form action="configurar.php" method="post" id ="datos-usuario">
-            <select name="tema" id="tema">
-                <?php
-                    if($resultado = $conexion->query("SELECT * FROM estilos")) {
-                        while($fila = $resultado->fetch_assoc()) {
-                            $predeterminado = $fila["Nombre"] == $tema ? 'selected' : '';
-                            echo '<option ' . $predeterminado . ' value="' . $fila["Nombre"] . '">' . $fila["Descripcion"] . '</button>';
-                        }
-                        cerrarConexion($resultado, $conexion);
-                    }
-                ?>
-            </select>
-            <button type="submit">Cargar estilo</button>
-        </form>
-    </main>
-    <?php
-        require_once "inc/footer.php";
-    ?>
-</body>
-</html>
