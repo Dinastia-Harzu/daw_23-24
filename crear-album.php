477c1a04ceda773968f140a632d2cf4a57734b31
Romboscuro <mgv76@alu.ua.es>
2023-12-12 18:26:08 +0100

    crear album mvc completado

--- crear-album.php
+++ crear-album.php
@@ -1,37 +1,3 @@
 <?php
-    require_once "helpers/funciones.php";
-    session_start();
-
-    generarHead('crear-album');
+require_once('views/crear-album-view.php');
 ?>
-<body>
-    <?php
-        require_once "inc/header.php";
-    ?>
-    <main>
-        <section id="tab-crear-album">
-            <h1>Crea tu álbum</h1>
-            <form action="usuario.php">
-                <div class="omrs-input-group">
-                    <label class="omrs-input-filled">
-                        <input type="text" placeholder=" " name="titulo" id="titulo-album">
-                        <span class="omrs-input-label">Titulo</span>
-                    </label>
-                </div>
-                <div class="omrs-input-group">
-                    <label class="omrs-input-filled">
-                        <textarea></textarea>
-                        <span class="omrs-input-label">Descripción</span>
-                    </label>
-                </div>
-                <div>
-                    <button type="submit" id="btn-crear">Crear álbum</button>
-                </div>
-            </form>
-        </section>
-    </main>
-    <?php
-        require_once "inc/footer.php";
-    ?>
-</body>
-</html>
