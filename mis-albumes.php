af5d8ab50b1bd0514de6d832753d528e0e37f282
Romboscuro <mgv76@alu.ua.es>
2023-12-12 18:16:58 +0100

    mis albumes mvc completado

--- mis-albumes.php
+++ mis-albumes.php
@@ -1,46 +1,4 @@
 <?php
-    require_once "helpers/funciones.php";
-    session_start();
-
-    generarHead('mis-albumes', 'respuesta-usuario');
-
-    $conexion = abrirConexion();
+require_once('db/db.php');
+require_once('controllers/mis-albumes-controller.php');
 ?>
-<body>
-    <?php
-        require_once "inc/header.php";
-    ?>
-    <main>
-        <h2>Mis álbumes</h2>
-        <div id="datos-usuario">
-            <?php
-                $sql = "
-                    SELECT *
-                    FROM albumes
-                    WHERE Usuario = {$_GET["id"]}
-                ;";
-                if($resultado = $conexion->query($sql)) {
-                    while($fila = $resultado->fetch_assoc()) {
-                        echo <<<hereDOC
-                            <h3>
-                                <a href = "ver-album-priv.php?id={$fila["IdAlbum"]}">{$fila["Titulo"]}</a>
-                            </h3>
-                            <p>{$fila["Descripcion"]}</p>
-                            <hr>
-                        hereDOC;
-                    }
-                    cerrarConexion($resultado, $conexion);
-                }
-                echo <<<hereDOC
-                    <p>
-                        <a href = "mis-fotos.php?id={$_GET["id"]}">Ver todas las fotos</a>
-                    </p>
-                hereDOC;
-            ?>
-        </div>
-    </main>
-    <?php
-        require_once "inc/footer.php";
-    ?>
-</body>
-</html>
